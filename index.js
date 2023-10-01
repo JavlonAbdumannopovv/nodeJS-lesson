5e0a7474014e1a4316b6e37813b147f91ae498d4
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2023-10-01 13:34:18 +0500

    Path Module

---
+++ index.js
@@ -0,0 +1,5 @@
+// console.log("papkagacha bo`lgan yo`l", __dirname);
+// console.log("faylgacha bo`lgan yo`l", __filename);
+
+
+
---
+++ modules/path.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+// console.log(path.basename(__filename)); // biz turgan FAYL nomini ko`rsatadi
+// console.log(path.basename(__dirname)); // biz turgan PAPKA nomini ko`rsatadi
+
+// console.log(path.dirname(__dirname)); // biz turgan Papkagacha bo`lgan yo`lni ko`rsatadi
+// console.log(path.dirname(__filename)); // biz turgan  Faylgacha bo`lgan yo`lni ko`rsatadi
+
+// console.log(path.extname(__filename)); // biz turgan Faylning Kengaytmasini korsatadi misol: .js
+
+// console.log(path.parse(__filename)); // biz turgan Fayl haqida malumotlar(root, base, ext, name, dir) ni olib beradi
+// console.log(path.parse(__dirname)); // biz turgan Papka haqida malumotlar(root, base, ext, name, dir) ni olib beradi
+
+// console.log(path.join(__dirname, "templates", "index.html")); // Fayllarni bir biriga qoshib beradi yoki yangi fayl ochib beradi
+
+
+
+
